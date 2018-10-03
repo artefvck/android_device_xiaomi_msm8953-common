57ab3c212c2c393a6ba606d12ec33149aadcf80b
artefvck <artefvck07@gmail.com>
2018-10-03 13:36:29 +0700

    msm8953-common : update config.fs from CAF
    Change-Id: I06fff55e4fcf7dbcc8dc18422b1102d06a2facd4
    Signed-off-by: artefvck <artefvck07@gmail.com>

--- config.fs
+++ config.fs
@@ -94,6 +94,12 @@ user: AID_SYSTEM
 group: AID_RADIO
 caps: NET_BIND_SERVICE
 
+[vendor/bin/imsrcsd]
+mode: 0755
+user: AID_SYSTEM
+group: AID_RADIO
+caps: NET_BIND_SERVICE BLOCK_SUSPEND WAKE_ALARM
+
 [vendor/bin/cnd]
 mode: 0755
 user: AID_SYSTEM
@@ -133,6 +139,18 @@ caps: NET_BIND_SERVICE BLOCK_SUSPEND
 [firmware/]
 mode: 0771
 user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[firmware/image/*]
+mode: 0771
+user: AID_SYSTEM
+group: AID_SYSTEM
+caps: 0
+
+[vendor/firmware_mnt/image/*]
+mode: 0771
+user: AID_ROOT
 group: AID_SYSTEM
 caps: 0
 
